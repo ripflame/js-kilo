c98a43c53fa72829f28bcd8f7757a3d531a5197b
Gilberto Leon <ripflame@gmail.com>
2025-09-26 23:36:04 -0600

    Chunk drawing the tildes

--- kilo.js
+++ kilo.js
@@ -1,38 +1,29 @@
+/*** includes ***/
 import { exit, stdin, stdout } from "node:process";
 import fs from "node:fs";
+/****************/
 
-const editorProcessKeypress = {
-  17: () => {
-    exit(0);
-  },
+/*** defines ***/
+/***************/
+
+/*** data ***/
+const editorConfig = {
+  screenRows: 0,
+  screenCols: 0,
 };
+/************/
 
-function editorRefreshScreen() {
-  stdout.write("\x1b[2J", "utf8");
-  stdout.write("\x1b[H", "utf8");
-}
-
-function main() {
-  if (!stdin.isTTY) {
-    console.error("No piping.");
-    exit(1);
-  }
-
-  stdin.on("data", (data) => {
-    editorRefreshScreen();
-    if (data.length === 1) {
-      (editorProcessKeypress[data[0]] || (() => {}))();
-    } else {
-    }
-  });
-
-  stdin.setRawMode(true);
+/*** terminal ***/
+function getWindowSize() {
+  const windowSize = stdout.getWindowSize();
+  editorConfig.screenCols = windowSize[0];
+  editorConfig.screenRows = windowSize[1];
 }
 
 function cleanup() {
-  stdout.write("\x1b[2K", "utf8");
-  stdout.write("\x1b[H", "utf8");
-  stdout.write("\x1b[?25h", "utf8");
+  stdout.write("\x1b[2K");
+  stdout.write("\x1b[H");
+  stdout.write("\x1b[?25h");
   if (stdin.isTTY) stdin.setRawMode(false);
 }
 
@@ -63,5 +54,59 @@ process.on("unhandledRejection", () => {
   cleanup();
   exit(1);
 });
+/****************/
+
+/*** output ***/
+function editorDrawRows() {
+  const tildes = [];
+  for (let i = 0; i < editorConfig.screenRows; i++) {
+    tildes.push("~");
+    if (i < editorConfig.screenRows - 1) {
+      tildes.push("\r\n");
+    }
+  }
+  stdout.write(tildes.join(""));
+}
+
+function editorRefreshScreen() {
+  stdout.write("\x1b[2J");
+  stdout.write("\x1b[H");
+  editorDrawRows();
+  stdout.write("\x1b[H");
+}
+/**************/
+
+/*** input ***/
+const editorProcessKeypress = {
+  17: () => {
+    exit(0);
+  },
+};
+/*************/
+
+/*** init ***/
+function initEditor() {
+  getWindowSize();
+}
+
+function main() {
+  if (!stdin.isTTY) {
+    console.error("No piping.");
+    exit(1);
+  }
+
+  stdin.on("data", (data) => {
+    editorRefreshScreen();
+    if (data.length === 1) {
+      (editorProcessKeypress[data[0]] || (() => {}))();
+    } else {
+    }
+  });
+
+  initEditor();
+  editorRefreshScreen();
+  stdin.setRawMode(true);
+}
+/************/
 
 main();
